9435e1e43936ea23fe701c4789bd66998594edab
zhouyc <yongchengzhou@hotmail.com>
2015-08-11 07:09:46 +0800

    添加管理控制台的页面Layout及相关文件［ＡｄｍｉｎＬＴＥ］

--- portal/src/main/webapp/main.js
+++ portal/src/main/webapp/main.js
@@ -1,12 +1,15 @@
 require.config({
     paths : {
-        angular : 'bower_components/angular/angular',
-        uiRouter : 'bower_components/angular-ui-router/release/angular-ui-router',
+        angular : 'bower_components/angular/angular.min',
+        angularResource:'bower_components/angular-resource/angular-resource.min',
+        uiRouter : 'bower_components/angular-ui-router/release/angular-ui-router.min',
         jquery : 'bower_components/jquery/dist/jquery.min',
         twitter: 'bower_components/bootstrap/dist/js/bootstrap.min',
         domReady : 'bower_components/requirejs-domready/domReady',
         loginModule : 'modules/login/loginModule',
-        brokerModule : 'modules/broker/brokerModule'
+        brokerModule : 'modules/broker/brokerModule',
+        AdminLTE : 'js/app.min',
+        Chart : 'bower_components/Chart.js/Chart.min'
     },
     shim : {
         twitter : {
@@ -19,13 +22,23 @@ require.config({
         uiRouter : {
             deps : ['angular'],
             exports : 'uiRouter'
+        },
+        angularResource : {
+            deps : ['angular']
+        },
+        AdminLTE : {
+            deps : ['jquery', 'twitter'],
+            exports : '$.AdminLTE'
         }
     }
 });
 
 require(['angular',
-    'bootstrap',
-    'app'
-], function(angular, app) {
+         'app',
+         'AdminLTE',
+         'Chart'
+], function(angular, app, Chart) {
     'use strict';
+    var chartJs = Chart.noConflict();
+
 });
